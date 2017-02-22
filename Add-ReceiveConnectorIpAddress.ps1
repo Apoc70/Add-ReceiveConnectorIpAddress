cae1aace0126f5f9fa4880d7619829973e58d118
Thomas Stensitzki <thomas@mcsmemail.de>
2017-02-22 20:21:58 +0100

    PowerShell cleanup Close #1

--- Add-ReceiveConnectorIpAddress.ps1
+++ Add-ReceiveConnectorIpAddress.ps1
@@ -7,12 +7,12 @@
     THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE  
     RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER. 
 
-    Version 1.2, 2016-05-18
+    Version 1.3, 2017-02-22
 
     Please send ideas, comments and suggestions to support@granikos.eu 
 
     .LINK 
-    More information can be found at http://scripts.granikos.eu
+    http://scripts.granikos.eu
 
     .DESCRIPTION 
     This script adds a given IP address to an existing Receive Connector or reads an
@@ -35,6 +35,7 @@
     1.0 Initial community release 
     1.1 Sorting for Exchange servers added
     1.2 PowerShell hygiene
+    1.3 PowerShell hygiene - Part II
 
     .PARAMETER ConnectorName  
     Name of the connector the new IP addresses should be added to  
@@ -54,14 +55,13 @@
 #> 
 param(
 	[parameter(Mandatory=$true,HelpMessage='Name of the Receive Connector')]
-		[string] $ConnectorName,
+	[string] $ConnectorName,
 	[parameter(Mandatory=$true,HelpMessage='Name of the input file name containing IP addresses')]
-		[string] $FileName,
-    [parameter(Mandatory=$false,HelpMessage='View entire Active Directory forest (default FALSE)')]
-        [boolean] $ViewEntireForest = $false
+	[string] $FileName,
+  [boolean] $ViewEntireForest = $false
 )
 
-Set-StrictMode -Version Latest
+# Set-StrictMode -Version Latest
 
 $tmpFileFolderName = 'ReceiveConnectorIpAddresses'
 $tmpFileLocation = ''
@@ -72,68 +72,70 @@ $timeStamp = Get-Date -Format 'yyyy-MM-dd HHmmss'
 
 function Test-LogPath {
     $script:tmpFileLocation = Join-Path -Path $PSScriptRoot -ChildPath $tmpFileFolderName
-    if(-not (Test-Path $script:tmpFileLocation)) {
-        Write-Verbose 'New file folder created'
+    if(-not (Test-Path -Path $script:tmpFileLocation)) {
+        Write-Verbose -Message 'New file folder created'
         New-Item -ItemType Directory -Path $script:tmpFileLocation -Force | Out-Null
     }
 }
 
 function Test-ReceiveConnector {
+    [CmdletBinding()]
     param(
         [string]$Server
     )
 
-    Write-Verbose "Checking Server: $Server"
+    Write-Verbose -Message ('Checking Server: {0}' -f $Server)
 
     # Fetch receive connector from server
     $targetRC = Get-ReceiveConnector -Server $Server | Where-Object{$_.name -eq $ConnectorName} -ErrorAction SilentlyContinue
 
     if($targetRC -ne $null) {
-        Write-Verbose "Found connector $ConnectorName on server $Server"
+        Write-Verbose -Message ('Found connector {0} on server {1}' -f $ConnectorName, $Server)
 	    Write-ConnectorIpRanges -ReceiveConnector $targetRC
     }
     else {
-        Write-Output "INFO: Connector $ConnectorName NOT found on server $Server"
+        Write-Output -InputObject ('INFO: Connector {0} NOT found on server {1}' -f $ConnectorName, $Server)
     }
 }
 
 function Write-ConnectorIpRanges {
+    [CmdletBinding()]
     param (
         $ReceiveConnector
     )
     # Create a list of currently configured IP ranges 
     $tmpRemoteIpRanges = ''
     foreach ( $remoteIpRange in ($ReceiveConnector).RemoteIPRanges ) {
-        $tmpRemoteIpRanges += "`r`n$remoteIpRange"			
+        $tmpRemoteIpRanges += ("`r`n{0}" -f $remoteIpRange)			
 	}
 
-    Write-Verbose $tmpFileLocation
+    Write-Verbose -Message $tmpFileLocation
     
     # Save current remote IP ranges for connector to disk
-    $fileIpRanges = ("$tmpFileLocation\$ConnectorName-$timeStamp-Export.txt").ToUpper()
-    Write-Verbose "Saving current IP ranges to: $fileIpRanges"
-    Write-Output $tmpRemoteIpRanges | Out-File -FilePath $fileIpRanges -Force -Encoding UTF8
+    $fileIpRanges = (('{0}\{1}-{2}-Export.txt' -f $tmpFileLocation, $ConnectorName, $timeStamp)).ToUpper()
+    Write-Verbose -Message ('Saving current IP ranges to: {0}' -f $fileIpRanges)
+    Write-Output -InputObject $tmpRemoteIpRanges | Out-File -FilePath $fileIpRanges -Force -Encoding UTF8
 
     # Fetch new IP ranges from disk
     $newIpRangesFileContent = ''
-    if(Test-Path $FileName) {
-	    Write-Verbose "Reading file $FileName"
+    if(Test-Path -Path $FileName) {
+	    Write-Verbose -Message ('Reading file {0}' -f $FileName)
 	    $newIpRangesFileContent = Get-Content -Path $FileName
     }
 
     # add new IP ranges, if file exsists and had content
     if($newIpRangesFileContent -ne ''){
         foreach ($newIpRange in $newIpRangesFileContent ){
-	        Write-Verbose "Checking new Remote IP range $newIpRange in $fileIpRanges"
+	        Write-Verbose -Message ('Checking new Remote IP range {0} in {1}' -f $newIpRange, $fileIpRanges)
             # Check if new remote IP range already exists in configure remote IP range of connector
 	        $ipSearch = (Select-String -Pattern $newIpRange -Path $fileIpRanges )
 	        if ($ipSearch -ne $null ){
                 # Remote IP range exists, nothing to do here
-                Write-Output "Remote IP range [$newIpRange] already exists"
+                Write-Output -InputObject ('Remote IP range [{0}] already exists' -f $newIpRange)
 		    }
 		    else {
                 # Remote IP range does not exist, add range to receive connector object
-	            Write-Output "Remote IP range [$newIpRange] will be added to $ConnectorName" 
+	            Write-Output -InputObject ('Remote IP range [{0}] will be added to {1}' -f $newIpRange, $ConnectorName) 
 	            $ReceiveConnector.RemoteIPRanges += $newIpRange
 	        }
         }
@@ -145,7 +147,7 @@ function Write-ConnectorIpRanges {
 # MAIN -------------------------------------------------------
 
 if($ViewEntireForest) {
-    Write-Verbose "Setting ADServerSettings -ViewEntireForest $true"
+    Write-Verbose -Message ('Setting ADServerSettings -ViewEntireForest {0}' -f $true)
     Set-ADServerSettings -ViewEntireForets $true
 }
 
@@ -155,6 +157,6 @@ Test-LogPath
 $allExchangeServers = Get-ExchangeServer | Where-Object{($_.AdminDisplayVersion.Major -eq 15) -and ([string]$_.ServerRole).Contains('ClientAccess')} | Sort-Object
 
 foreach($Server in $AllExchangeServers) {
-    Write-Output "Checking receive connector $ConnectorName on server $Server"
+    Write-Output -InputObject ('Checking receive connector {0} on server {1}' -f $ConnectorName, $Server)
     Test-ReceiveConnector -Server $Server
 }
